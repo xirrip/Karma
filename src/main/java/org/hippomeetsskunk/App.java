22d2d27f87b699019bd2b4925d1e3931d0b7227f
Xirrip <xirrip@gmail.com>
2015-08-14 16:53:52 +0200

    Initial commit

---
+++ src/main/java/org/hippomeetsskunk/App.java
@@ -0,0 +1,13 @@
+package org.hippomeetsskunk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
